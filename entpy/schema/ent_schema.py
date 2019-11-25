b2b0a89c8e0d8e2026cbe03824d9ca2a23627c7e
Shahar Mintz <smintz@protoconf.com>
2019-11-25 15:57:50 +0000

    Adding mutators

--- entpy/schema/ent_schema.py
+++ entpy/schema/ent_schema.py
@@ -1,5 +1,6 @@
 from entpy.base.base import EntBase
 from entpy.storage.local_data_storage import LocalDataStorage
+from mutator import EntMutationData, EntMutationView, EntMutationBuilder
 
 class EntSchema:
     def __init__(self):
@@ -38,7 +39,62 @@ class EntSchema:
                 return edge.schema.getEntFactory().gen(id)
             setattr(constructor, "gen" + name, func)
 
-        return constructor(self.getName(), self.getFields())
+        return constructor
+
+    def getEntFactory(self):
+        storage = self.getStorage()
+        Ent = self.getEntClass()
+        fields = self.getFields()
+
+        def func(ids):
+            items = storage.select(ids)
+            entities = dict()
+            for key in items:
+                if not items[key]:
+                    continue
+                rawData = dict()
+                for name in fields:
+                    field = fields[name]
+                    rawData[name] = items[key][field.storage_key]
+                entities[key] = Ent(key, rawData)
+        return func
+
+    def getMutationBuilderClass(self):
+        class constructor(EntMutationBuilder):
+            pass
+
+        fields = self.getFields()
+        for name in fields.keys():
+            field = fields[name]
+
+            def func(self, value):
+                return self.setField(name, field.assertx(value))
+
+            setattr(constructor, "set" + name, func)
+
+        return constructor
+
+    def getMutationViewClass(self):
+        class constructor(EntMutationView):
+            pass
+
+        fields = self.getFields()
+        for name in fields.keys():
+            def func(self):
+                return self.getOldField(name)
+            setattr(constructor, "getOld" + name, func)
+
+            def func(self):
+                return self.getNewField(name)
+            setattr(constructor, "getNew" + name, func)
+
+            def func(self):
+                return self.getNewOrOldField(name)
+            setattr(constructor, "getNewOrOld" + name, func)
+        return constructor
+
+
+
 
 
 class EntSchemaField:
---
+++ entpy/schema/mutator.py
@@ -0,0 +1,76 @@
+class EntMutationData:
+    def __init__(self, entity):
+        self._entity = entity
+        self._fields = dict()
+
+    def getEntity(self):
+        return self._entity
+
+    def setField(self, name, value):
+        self._fields[name] = value
+
+    def hasField(self, name):
+        return hasattr(self, name)
+
+    def getField(self, name):
+        return self._fields[name]
+
+    def getAvailableFields(self):
+        return self._fields.keys()
+
+class EntMutationBuilder:
+    def __init__(self, schema, operation, data):
+        self._schema = schema
+        self._operation = operation
+        self._data = data
+
+    def setField(self, name, value):
+        self._data.setField(name, value)
+        return self
+
+    def save(self):
+        schema = self._schema
+        View = schema.getMutationViewClass()
+        view = View(self._data)
+
+        # TODO: triggers, validators and observers
+
+        fields = scema.getFields()
+        rawData = dict()
+
+        for name in fields.keys():
+            field = fields[name]
+            rawData[field.storage_key] = view.getNewOrOldField(name)
+
+        storage = schema.getStorage()
+        entity = self._data.getEntity()
+
+        if self._operation == CREATE:
+            write = storage.create(rawData)
+        if self._operation == UPDATE:
+            write = storage.update(entity.getID(), rawData)
+        if self._operation == DELETE:
+            write  = storage.delete(entity.getID())
+
+        factory = schema.getEntFactory()
+        return factory(write)
+
+class EntMutationView:
+    def __init__(self, data):
+        self._data = data
+
+    def getID(self):
+        return self._data.getEntity().getID()
+
+    def getOldField(self, name):
+        return getattr(self._data.getEntity(), "get" + name)()
+
+    def getNewField(self, name):
+        if self._data.hasField(name):
+            return self._data.getField(name)
+        return None
+
+    def getNewOrOldField(self, name):
+        if self._data.hasField(name):
+            return this._data.getField(name)
+        return this.getOldField(name)
