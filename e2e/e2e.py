e703d197900aebd31a606c8a8a0e19f114645b46
Shahar Mintz <smintz@protoconf.com>
2019-12-05 09:13:21 +0000

    Reuse e2e test over multiple storages

---
+++ e2e/e2e.py
@@ -0,0 +1,119 @@
+import unittest
+from entpy.schema.ent_schema import (
+    EntSchema,
+    StringSchemaField,
+    NumberSchemaField,
+    EntSchemaEdge,
+)
+
+import random
+import string
+
+
+def randomString(stringLength=10):
+    letters = string.ascii_lowercase
+    return "".join(random.choice(letters) for i in range(stringLength))
+
+
+MYSTRING = randomString()
+
+
+class IntegrationTestFactory(unittest.TestCase):
+    def setUp(self, storage_engine, connection):
+        self.storage_engine = storage_engine
+        self.connection = connection
+
+    def getEntLocationSchema(self):
+        storage = self.storage_engine
+        conn = self.connection
+
+        class EntLocationSchema(EntSchema):
+            @staticmethod
+            def getName():
+                return "location_" + MYSTRING
+
+            @staticmethod
+            def getFields():
+                return {
+                    "City": StringSchemaField("city"),
+                    "Country": StringSchemaField("country"),
+                }
+
+            def setStorage(self):
+                return storage(conn, self)
+
+        return EntLocationSchema
+
+    def getEntPersonSchema(self):
+        storage = self.storage_engine
+        conn = self.connection
+        EntLocationSchema = self.getEntLocationSchema()
+        LocationSchema = EntLocationSchema()
+
+        class EntPersonSchema(EntSchema):
+            @staticmethod
+            def getName():
+                return "people_" + MYSTRING
+
+            @staticmethod
+            def getFields():
+                return {
+                    "Name": StringSchemaField("name"),
+                    "Age": NumberSchemaField("age"),
+                    "LocationID": NumberSchemaField("location_id"),
+                }
+
+            @staticmethod
+            def getEdges():
+                return {"Location": EntSchemaEdge("LocationID", LocationSchema)}
+
+            def setStorage(self):
+                return storage(conn, self)
+
+        return EntPersonSchema
+
+    def helper(self):
+        EntLocationSchema = self.getEntLocationSchema()
+        LocationSchema = EntLocationSchema()
+        EntLocation = LocationSchema.getEntClass()
+        EntLocationMutator = LocationSchema.getMutator()
+        EntLocationFactory = LocationSchema.getEntFactory()
+        EntPersonSchema = self.getEntPersonSchema()
+        PersonSchema = EntPersonSchema()
+        EntPersonMutator = PersonSchema.getMutator()
+        EntPersonFactory = PersonSchema.getEntFactory()
+
+        tlv = EntLocationMutator.create()
+        tlv.setCity("Tel Aviv")
+        tlv.setCountry("Israel")  # .save()
+        result = tlv.save()
+
+        self.assertEqual(result.getCountry(), "Israel")
+        self.assertEqual(result.getCity(), "Tel Aviv")
+
+        u = EntLocationMutator.update(result)
+
+        u.setCity("Holon")
+        result = u.save()
+        self.assertEqual(result.getCity(), "Holon")
+
+        read = EntLocationFactory.gen(1)
+        self.assertEqual(read.getCity(), "Holon")
+
+        smintz = (
+            EntPersonMutator.create()
+            .setName("Shahar")
+            .setAge(35)
+            .setLocationID(result.getID())
+            .save()
+        )
+        lior = (
+            EntPersonMutator.create()
+            .setName("Lior")
+            .setAge(30)
+            .setLocationID(result.getID())
+            .save()
+        )
+        self.assertEqual(lior.getID(), 2)
+        self.assertEqual(smintz.getName(), "Shahar")
+        self.assertEqual(smintz.genLocation().getCountry(), result.getCountry())
--- e2e/postgresql_test.py
+++ e2e/postgresql_test.py
@@ -1,115 +1,25 @@
-import unittest
 import psycopg2
-import random
-import string
-
-
-def randomString(stringLength=10):
-    """Generate a random string of fixed length """
-    letters = string.ascii_lowercase
-    return "".join(random.choice(letters) for i in range(stringLength))
-
-
-from entpy.schema.ent_schema import (
-    EntSchema,
-    StringSchemaField,
-    NumberSchemaField,
-    EntSchemaEdge,
-)
+import unittest
 from entpy.storage.sql.sql_storage import PostgreSQLStorage
+from e2e import IntegrationTestFactory
+import socket
+
+host = "postgres"
+try:
+    socket.gethostbyname(host)
+except:
+    host = "127.0.0.1"
 
 
-MYSTRING = randomString()
-conn = psycopg2.connect(
-    host="postgres", user="postgres", password="my-secret-pw", database="entpy"
-)
+conn = psycopg2.connect(host=host, user="postgres", password="my-secret-pw")
 
 
-class EntLocationSchema(EntSchema):
-    @staticmethod
-    def getName():
-        return "location_" + MYSTRING
+class SqliteTest(IntegrationTestFactory):
+    def setUp(self):
+        super(SqliteTest, self).setUp(PostgreSQLStorage, conn)
 
-    @staticmethod
-    def getFields():
-        return {
-            "City": StringSchemaField("city"),
-            "Country": StringSchemaField("country"),
-        }
-
-    def setStorage(self):
-        return PostgreSQLStorage(conn, self)
-
-
-LocationSchema = EntLocationSchema()
-EntLocation = LocationSchema.getEntClass()
-EntLocationMutator = LocationSchema.getMutator()
-EntLocationFactory = LocationSchema.getEntFactory()
-
-
-class EntPersonSchema(EntSchema):
-    @staticmethod
-    def getName():
-        return "people_" + MYSTRING
-
-    @staticmethod
-    def getFields():
-        return {
-            "Name": StringSchemaField("name"),
-            "Age": NumberSchemaField("age"),
-            "LocationID": NumberSchemaField("location_id"),
-        }
-
-    @staticmethod
-    def getEdges():
-        return {"Location": EntSchemaEdge("LocationID", LocationSchema)}
-
-    def setStorage(self):
-        return PostgreSQLStorage(conn, self)
-
-
-PersonSchema = EntPersonSchema()
-EntPersonMutator = PersonSchema.getMutator()
-EntPersonFactory = PersonSchema.getEntFactory()
-
-
-class EntSchemaTest(unittest.TestCase):
-    def test_schema(self):
-
-        tlv = EntLocationMutator.create()
-        tlv.setCity("Tel Aviv")
-        tlv.setCountry("Israel")  # .save()
-        result = tlv.save()
-
-        self.assertEqual(result.getCountry(), "Israel")
-        self.assertEqual(result.getCity(), "Tel Aviv")
-
-        u = EntLocationMutator.update(result)
-
-        u.setCity("Holon")
-        result = u.save()
-        self.assertEqual(result.getCity(), "Holon")
-
-        read = EntLocationFactory.gen(1)
-        self.assertEqual(read.getCity(), "Holon")
-
-        smintz = (
-            EntPersonMutator.create()
-            .setName("Shahar")
-            .setAge(35)
-            .setLocationID(result.getID())
-            .save()
-        )
-        lior = (
-            EntPersonMutator.create()
-            .setName("Lior")
-            .setAge(30)
-            .setLocationID(result.getID())
-            .save()
-        )
-        self.assertEqual(lior.getID(), 2)
-        self.assertEqual(smintz.getName(), "Shahar")
-        self.assertEqual(smintz.genLocation().getCountry(), result.getCountry())
+    def test_sqlite(self):
+        self.helper()
 
 
 if __name__ == "__main__":
